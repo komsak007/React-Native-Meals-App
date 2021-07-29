1438beac36f34600d34c5819de479fa459077d50
komsak007 <lert_zodiac@hotmail.com>
2021-07-29 18:27:57 +0700

    First Commit Meals app

---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+
+const CategoryMealsScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <Text>Category Meals Screen</Text>
+      <Button
+        title="Go to DetailScreen"
+        onPress={() => {
+          props.navigation.navigate({ routeName: "MealDetail" });
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default CategoryMealsScreen;
