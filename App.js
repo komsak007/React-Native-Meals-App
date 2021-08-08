bb4f18a42e2cb33aabcf5a06327d4e1c96de96c4
komsak007 <lert_zodiac@hotmail.com>
2021-08-08 22:20:55 +0700

    add favorite meals with redux state

--- App.js
+++ App.js
@@ -3,10 +3,19 @@ import { StyleSheet, Text, View } from "react-native";
 import { useFonts } from "expo-font";
 import AppLoading from "expo-app-loading";
 import { enableScreens } from "react-native-screens";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
 
 import MealsNavigator from "./navigation/MealsNavigator";
+import mealsReducer from "./store/reducers/meals";
 
 enableScreens();
+
+const rootReducer = combineReducers({
+  meals: mealsReducer,
+});
+
+const store = createStore(rootReducer);
 
 // const fetchFonts = () => {
 //   return Font.loadAsync({
@@ -25,7 +34,11 @@ export default function App() {
     return <AppLoading />;
   }
 
-  return <MealsNavigator />;
+  return (
+    <Provider store={store}>
+      <MealsNavigator />
+    </Provider>
+  );
 }
 
 const styles = StyleSheet.create({
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,14 +1,27 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { View, StyleSheet } from "react-native";
+import DefaultText from "../components/DefaultText";
 
-import { CATEGORIES, MEALS } from "../data/dummy-data";
+import { CATEGORIES } from "../data/dummy-data";
 import MealList from "../components/MealList";
 
 const CategoryMealsScreen = (props) => {
   const catId = props.navigation.getParam("categoryId");
 
-  const displayedMeals = MEALS.filter(
+  const avalibleMeals = useSelector((state) => state.meals.filteredMeals);
+
+  const displayedMeals = avalibleMeals.filter(
     (meal) => meal.categoryIds.indexOf(catId) >= 0
   );
+
+  if (displayedMeals.length === 0) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No meals founds, maybe check your filters?</DefaultText>
+      </View>
+    );
+  }
 
   return <MealList listData={displayedMeals} navigation={props.navigation} />;
 };
@@ -23,6 +36,12 @@ CategoryMealsScreen.navigationOptions = (navigationData) => {
   };
 };
 
-// const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default CategoryMealsScreen;
